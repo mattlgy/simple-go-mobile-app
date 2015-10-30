aa8db5d21a152899824eb82c38ba026807a520fd
Matt Liegey <mtliegey@gmail.com>
2015-10-29 21:42:25 -0400

    First commit

---
+++ main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"encoding/binary"
+	"log"
+	"time"
+
+	"golang.org/x/mobile/app"
+	"golang.org/x/mobile/exp/sensor"
+	"golang.org/x/mobile/event/lifecycle"
+	"golang.org/x/mobile/event/paint"
+	"golang.org/x/mobile/event/size"
+	"golang.org/x/mobile/event/touch"
+	"golang.org/x/mobile/exp/app/debug"
+	"golang.org/x/mobile/exp/f32"
+	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/gl"
+)
+
+var (
+	images   *glutil.Images
+	fps      *debug.FPS
+	program  gl.Program
+	position gl.Attrib
+	offset   gl.Uniform
+	color    gl.Uniform
+	buf      gl.Buffer
+
+	green  float32
+
+	posX float32 = 0.5
+	posY float32 = 0.5
+
+	dX float32 = 0.0
+	dY float32 = 0.0
+)
+
+func main() {
+	app.Main(func(a app.App) {
+		// have sensor notify the app
+		sensor.Notify(a)
+		// turn on the gryoscope sensor
+		sensor.Enable(sensor.Gyroscope, time.Duration(100)*time.Millisecond)
+		var glctx gl.Context
+		var sz size.Event
+		// continuously loop through OS/App events
+		for e := range a.Events() {
+			// switch on the events type
+			switch e := a.Filter(e).(type) {
+			case lifecycle.Event:
+				switch e.Crosses(lifecycle.StageVisible) {
+				case lifecycle.CrossOn:
+					glctx, _ = e.DrawContext.(gl.Context)
+					onStart(glctx)
+					a.Send(paint.Event{})
+				case lifecycle.CrossOff:
+					onStop(glctx)
+					glctx = nil
+				}
+			case size.Event:
+				sz = e
+			case paint.Event:
+				if glctx == nil || e.External {
+					// As we are actively painting as fast as
+					// we can (usually 60 FPS), skip any paint
+					// events sent by the system.
+					continue
+				}
+
+				onPaint(glctx, sz)
+				a.Publish()
+				// Drive the animation by preparing to paint the next frame
+				// after this one is shown.
+				a.Send(paint.Event{})
+			case touch.Event:
+				// on tap, zero-out the velocity, "re-leveling"
+				dX = 0.0
+				dY = 0.0
+			case sensor.Event:
+				// add the force the gyroscope reads to the velocities
+				dX = dX + float32(e.Data[1]) / 500
+				dY = dY + float32(e.Data[0]) / 500
+			}
+		}
+	})
+}
+
+func onStart(glctx gl.Context) {
+	var err error
+	program, err = glutil.CreateProgram(glctx, vertexShader, fragmentShader)
+	if err != nil {
+		log.Printf("error creating GL program: %v", err)
+		return
+	}
+
+	buf = glctx.CreateBuffer()
+	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
+	glctx.BufferData(gl.ARRAY_BUFFER, triangleData, gl.STATIC_DRAW)
+
+	position = glctx.GetAttribLocation(program, "position")
+	color = glctx.GetUniformLocation(program, "color")
+	offset = glctx.GetUniformLocation(program, "offset")
+
+	images = glutil.NewImages(glctx)
+	fps = debug.NewFPS(images)
+}
+
+func onStop(glctx gl.Context) {
+	glctx.DeleteProgram(program)
+	glctx.DeleteBuffer(buf)
+	fps.Release()
+	images.Release()
+}
+
+func onPaint(glctx gl.Context, sz size.Event) {
+	glctx.ClearColor(1, 0, 0, 1)
+	glctx.Clear(gl.COLOR_BUFFER_BIT)
+
+	glctx.UseProgram(program)
+
+	green += 0.01
+	if green > 1 {
+		green = 0
+	}
+	glctx.Uniform4f(color, 0, green, 0, 1)
+
+	posX = posX + dX
+	if posX < 0 { posX = 0 }
+	if posX > 1 { posX = 1 }
+	posY = posY + dY
+	if posY < 0 { posY = 0 }
+	if posY > 1 { posY = 1 }
+	log.Printf("%v, %v", posX, posY)
+	glctx.Uniform2f(offset, posX, posY)
+
+	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
+	glctx.EnableVertexAttribArray(position)
+	glctx.VertexAttribPointer(position, coordsPerVertex, gl.FLOAT, false, 0, 0)
+	glctx.DrawArrays(gl.TRIANGLES, 0, vertexCount)
+	glctx.DisableVertexAttribArray(position)
+
+	fps.Draw(sz)
+}
+
+var triangleData = f32.Bytes(binary.LittleEndian,
+	0.0, 0.4, 0.0, // top left
+	0.0, 0.0, 0.0, // bottom left
+	0.4, 0.0, 0.0, // bottom right
+)
+
+const (
+	coordsPerVertex = 3
+	vertexCount     = 3
+)
+
+const vertexShader = `#version 100
+uniform vec2 offset;
+
+attribute vec4 position;
+void main() {
+	// offset comes in with x/y values between 0 and 1.
+	// position bounds are -1 to 1.
+	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
+	gl_Position = position + offset4;
+}`
+
+const fragmentShader = `#version 100
+precision mediump float;
+uniform vec4 color;
+void main() {
+	gl_FragColor = color;
+}`
